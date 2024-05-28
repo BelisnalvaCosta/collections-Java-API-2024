d61e145de29d9fa116c89ebc60f0843cb9d8279e
Belisnalva Costa <bella-costa@hotmail.com.br>
2024-05-27 23:56:55 -0300

    Initial commit

---
+++ src/list/operacoesBasicas/CarrinhoDeCompras.java
@@ -0,0 +1,84 @@
+package list.operacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+    //atributos
+    private List<Item> itemList;
+
+    public CarrinhoDeCompras() {
+        this.itemList = new ArrayList<>();
+    }
+
+    public void adicionarItem(String nome, double preco, int quantidade) {
+        Item item = new Item(nome, preco, quantidade);
+        this.itemList.add(item);
+    }
+
+    public void removerItem(String nome) {
+        List<Item> itensParaRemover = new ArrayList<>();
+        if (!itemList.isEmpty()) {
+            for (Item i : itemList) {
+                if (i.getNome().equalsIgnoreCase(nome)) {
+                    itensParaRemover.add(i);
+                }
+            }
+            itemList.removeAll(itensParaRemover);
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+    }
+
+    public double calcularValorTotal() {
+        double valorTotal = 0d;
+        if (!itemList.isEmpty()) {
+            for (Item item : itemList) {
+                double valorItem = item.getPreco() * item.getQtde();
+                valorTotal += valorItem; //valorTotal = valorTotal + valorItem;
+            }
+            return valorTotal;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public void exibirItens() {
+        if (!itemList.isEmpty()) {
+            System.out.println(this.itemList);
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "CarrinhoDeCompras{" +
+                "itens=" + itemList +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        // Criando uma instância do carrinho de compras
+        CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
+
+        // Adicionando itens ao carrinho
+        carrinhoDeCompras.adicionarItem("Caneta Azul", 5d, 5);
+        carrinhoDeCompras.adicionarItem("Papel Sulfit", 10d, 2);
+        carrinhoDeCompras.adicionarItem("Agenda", 30d, 1);
+        carrinhoDeCompras.adicionarItem("Caderno", 25d, 3);
+        carrinhoDeCompras.adicionarItem("Corretivo", 11d, 2);
+
+        // Exibindo os itens no carrinho
+        carrinhoDeCompras.exibirItens();
+
+        // Removendo um item do carrinho
+        carrinhoDeCompras.removerItem("Pepel Sulfit");
+
+        // Exibindo os itens atualizados no carrinho
+        carrinhoDeCompras.exibirItens();
+
+        // Calculando e exibindo o valor total da compra
+        System.out.println("O valor total da compra é = " + carrinhoDeCompras.calcularValorTotal());
+    }
+}
---
+++ src/list/operacoesBasicas/Item.java
@@ -0,0 +1,35 @@
+package list.operacoesBasicas;
+
+public class Item {
+    //atributos
+    private String nome;
+    private double preco;
+    private int qtde;
+
+    public Item(String nome, double preco, int qtde) {
+        this.nome = nome;
+        this.preco = preco;
+        this.qtde = qtde;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQtde() {
+        return qtde;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", qtde=" + qtde +
+                '}';
+    }
+}
